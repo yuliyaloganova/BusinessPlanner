edffe1b0f7c4c84286351fa8774e6adb2fbf6fa0
Arina Polikarpova <polikarpovaarina@gmail.com>
2025-02-14 19:16:21 +0400

    test commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        //test new string
+        //test new string_
     }
 }
